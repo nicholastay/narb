802ed84c80371d17820a7a6847b8ebf71a84a87e
Nicholas Tay <nkt@outlook.kr>
2020-02-02 13:47:04 +1100

    Add back cli args

--- narb.sh
+++ narb.sh
@@ -5,11 +5,19 @@
 # License: GNU GPLv3
 
 ### OPTIONS AND VARIABLES ###
+while getopts ":a:r:b:p:h" o; do case "${o}" in
+	h) printf "Optional arguments for custom use:\\n  -r: stow-based dotfiles repository (local file or url)\\n  -p: Dependencies and programs csv (local file or url)\\n  -a: AUR helper (must have pacman-like syntax)\\n  -h: Show this message\\n" && exit ;;
+	r) dotfilesrepo=${OPTARG} && git ls-remote "$dotfilesrepo" || exit ;;
+	b) repobranch=${OPTARG} ;;
+	p) progsfile=${OPTARG} ;;
+	a) aurhelper=${OPTARG} ;;
+	*) printf "Invalid option: -%s\\n" "$OPTARG" && exit ;;
+esac done
 
-dotfilesrepo="https://github.com/nicholastay/dotfiles.git"
-progsfile="https://raw.githubusercontent.com/nicholastay/dotfiles/master/progs.csv"
-aurhelper="yay"
-repobranch="master"
+[ -z "$dotfilesrepo" ] && dotfilesrepo="https://github.com/nicholastay/dotfiles.git"
+[ -z "$progsfile" ] && progsfile="https://raw.githubusercontent.com/nicholastay/dotfiles/master/progs.csv"
+[ -z "$aurhelper" ] && aurhelper="yay"
+[ -z "$repobranch" ] && repobranch="master"
 
 ### FUNCTIONS ###
 installpkg(){ pacman --noconfirm --needed -S "$1" >/dev/null 2>&1 ;}
